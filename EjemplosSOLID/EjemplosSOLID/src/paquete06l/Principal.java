470a78eb239687e6ff19f25cb8be22aa80f196af
paula226 <palopez25@utpl.edu.ec>
2024-07-10 16:02:53 -0500

    Actualizacion

--- EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
+++ EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
@@ -10,18 +10,22 @@ package paquete06l;
 public class Principal {
     public static void main(String[] args) {
       //  TarjetaCredito t1 = new TarjetaCredito("Visa, 110011001")
+        Banco ban = new Banco();
        TarjetaCredito t1 = new TarjetaCredito();
        t1.establecerNombre("Visa");
        t1.establecerNumero("11001101");
+     
+     
         
         MayorEdad representante = new MayorEdad("José", t1);
         
         MenorEdad menor = new MenorEdad("Felipe", representante);
+        ban.establecerNombre("Loja");
         
         
         System.out.printf("Nombre:%s - Tarjeta:%s\n",
                 menor.obtenerNombre(), 
                 menor.obtenerRepresentante().obtenerTarjeta().obtenerNumero());
-        
+                ban.obtenerNombre();
     }
 }
